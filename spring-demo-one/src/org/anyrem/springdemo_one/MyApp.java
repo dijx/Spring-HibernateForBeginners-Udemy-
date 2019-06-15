96a7229aba97c9ace3f3ae16285f488649c4998c
dijx <dijxie@gmail.com>
2019-06-16 01:03:01 +0200

    Setter Injection - begin

--- spring-demo-one/src/org/anyrem/springdemo_one/MyApp.java
+++ spring-demo-one/src/org/anyrem/springdemo_one/MyApp.java
@@ -1,14 +1,15 @@
-//package org.anyrem.springdemo_one;
-//
-//public class MyApp {
-//
-//    public static void main(String[] args) {
-////		Coach baseballCoach = new BaseballCoach();
-//        Coach myCoach = new TrackCoach();
-//
-//        System.out.println(myCoach.getDayilyWorkout());
-//
-//
-//    }
-//
-//}
+package org.anyrem.springdemo_one;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+//		Coach baseballCoach = new BaseballCoach();
+
+        Coach myCoach = new TrackCoach(() -> "This coach is working in compatibility mode");
+
+        System.out.println(myCoach.getDayilyWorkout());
+
+
+    }
+
+}
