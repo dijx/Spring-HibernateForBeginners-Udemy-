2677e25e2bfc1fd00a76835d9fce17bdc4b903ac
dijx <dijxie@gmail.com>
2019-06-28 14:44:59 +0200

    Hibernate one2one unidirectional - end

---
+++ hb-01-one-to-one-uni/src/main/java/org/anyrem/springdemo/hibernate/demo/AppSessionFactory.java
@@ -0,0 +1,26 @@
+package org.anyrem.springdemo.hibernate.demo;
+
+import org.anyrem.springdemo.hibernate.demo.entity.Instructor;
+import org.anyrem.springdemo.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AppSessionFactory {
+
+    public static Session getSession() {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        return session;
+    }
+
+    private AppSessionFactory() {
+    }
+}
