9b325c30f8b4a9ed516bfe0639f6661a6df1907c
dijx <dijxie@gmail.com>
2019-07-04 14:40:16 +0200

    Spring AOP- end

--- spring-demo-aop-pointcut-declarations/src/main/java/org/anyrem/springdemo/aop/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/main/java/org/anyrem/springdemo/aop/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,6 @@
 package org.anyrem.springdemo.aop.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
