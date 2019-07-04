b21f6ba76c402621372535bb4d336a50b9419864
dijx <dijxie@gmail.com>
2019-07-04 15:10:00 +0200

    Spring AOP - pointcut declarations - begin

--- spring-demo-aop-pointcut-declarations/src/main/java/org/anyrem/springdemo/aop/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/main/java/org/anyrem/springdemo/aop/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package org.anyrem.springdemo.aop.aspect;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -21,15 +22,23 @@ public class MyDemoLoggingAspect {
 //    @Before("execution(* add*(*, *))")
 //    @Before("execution(* org.anyrem.springdemo.aop.dao.MembershipDao.*(..))")
 //    @Before("execution(* org.anyrem.springdemo.aop.dao.AccountDao.do*(..))")
-    @Before("execution(* org.anyrem.springdemo.aop.dao.AccountDao.do*(*))")
-    public void deforeAddAddountAdvice() {
+//    @Before("execution(* org.anyrem.springdemo.aop.dao.AccountDao.do*(*))")
+
+
+    @Pointcut("execution(* org.anyrem.springdemo.aop.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+    @Before("forDaoPackage()")
+    public void beforeAddAddountAdvice() {
         System.out.println("ASPECT> executing @Before advice on addAccount()");
     }
 
 
+
     // public is optional, void is replaced with * = all return types
-    /*@After("execution(* addAccount*())")
+    @After("forDaoPackage()")
     public void AfterAddAccountAdvice() {
         System.out.println("ASPECT> executing @After advice on addAccount*()");
-    }*/
+    }
 }
