b74970c0eb794cafef006a28e9e6f48cc2b530ec
dijx <dijxie@gmail.com>
2019-07-05 14:22:41 +0200

    Spring AOP - aspects order - end

---
+++ spring-demo-aop-pointcut-declarations-aspects-ordering/src/main/java/org/anyrem/springdemo/aop/DemoConfig.java
@@ -0,0 +1,12 @@
+package org.anyrem.springdemo.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("org.anyrem.springdemo.aop")
+public class DemoConfig {
+
+}
