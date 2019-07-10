935e8a6843e88f78212ba1d6271dbb4c597d4bb7
dijx <dijxie@gmail.com>
2019-07-10 17:02:19 +0200

    SpringMVC security - cross site request forgery (CSRF)

---
+++ spring-demo-security-06-csrf/src/main/java/org/anyrem/springemo/mvc/security/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package org.anyrem.springemo.mvc.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
