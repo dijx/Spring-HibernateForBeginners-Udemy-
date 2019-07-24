b4f1b0ddf840bbe4833c8a56cf72f15ee22181cc
dijx <dijxie@gmail.com>
2019-07-24 18:14:26 +0200

    Thymeleaf - begin

---
+++ _SPRING_BOOT_THYMELEAF_/thymeleaf-demo/src/main/java/org/anyrem/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package org.anyrem.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+
+        model.addAttribute("theDate", LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
