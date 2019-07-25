54a9e3ef74b360e8cdcf734755c8a205a5f1e9e9
dijx <dijxie@gmail.com>
2019-07-25 14:33:10 +0200

    Springboot + Thymeleaf - CRUD app

---
+++ _SPRING_BOOT_THYMELEAF_CRUD_/thymeleaf-demo-employees-list-db/src/main/java/org/anyrem/springboot/thymeleafdemo/RunEmplList.java
@@ -0,0 +1,13 @@
+package org.anyrem.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunEmplList {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunEmplList.class, args);
+    }
+
+}
